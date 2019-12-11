acd924cba9bd48986ed8020fe4480776a0762d6b
LauGM <lmartin@animac.com.ar>
2019-12-11 11:01:18 -0300

    Segundo upload
    Segundo upload

---
+++ src/main/java/com/codeoftheweb/salvo/models/GamePlayerState.java
@@ -0,0 +1,13 @@
+package com.codeoftheweb.salvo.models;
+
+public enum GamePlayerState {
+        WAIT_OPPONENT,
+        PLACE_SHIPS,
+        WAIT_OPPONENT_SHIPS,
+        ENTER_SALVO,
+        WAIT_OPPONENT_SALVO,
+        WIN,
+        DRAW,
+        LOSE,
+        ERROR
+}
